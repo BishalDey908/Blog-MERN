80b6fc081082eb8a40ec29278de2e0fa2c738ff1
Bishal Dey <138598561+BishalDey908@users.noreply.github.com>
2024-01-15 11:45:45 +0530

    Update UI UpdatePost.jsx

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -82,15 +82,15 @@ const UpdatePost = () => {
               <form className="space-y-4 md:space-y-6" action="#">
              
                   <div>
-                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Add Title</label>
+                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Update Title</label>
                       <input name="title"  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"   required onChange={handleChange} value={blog.title} />
                   </div>
                   <div>
-                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Add ImageURL</label>
+                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Update ImageURL</label>
                       <input name="image"  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"   required onChange={handleChange} value={blog.image} />
                   </div>
                   <div>
-                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Add content</label>
+                      <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Update content</label>
                       <textarea  name="content"  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required  rows="7" onChange={handleChange} value={blog.content}/>
                   </div>
                   
@@ -98,7 +98,7 @@ const UpdatePost = () => {
                       
                       
                   </div>
-                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={handleSubmit}>Login to your account</button>
+                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={handleSubmit}>Update your post</button>
                   
               </form>
           </div>
